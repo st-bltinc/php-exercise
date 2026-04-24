39afb3a4ce30c5e077d1dff1a57618d805d35a8c
stblt <staniguchi@bltinc.co.jp>
2026-04-24 16:49:16 +0900

    feat: お問い合わせフォームの作成

--- html/index.php
+++ html/index.php
@@ -4,12 +4,20 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>test</title>
+    <title>お問い合わせ</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
-    <?php phpinfo(); ?>
+    <form method="POST" action="thanks.php">
+        <label for="name">名前</label><br>
+        <input type="text" id="name" name="name"><br>
+        <label for="email">メールアドレス</label><br>
+        <input type="email" id="email" name="email"><br>
+        <label for="password">パスワード</label><br>
+        <input type="password" id="password" name="password"><br>
+        <button type="submit" value="送信">送信</button>
+    </form>
 </body>
 
 </html>
