259dc93fa0d0dbfa6a6cac5977cadf5222ae3e55
stblt <staniguchi@bltinc.co.jp>
2026-04-24 17:54:53 +0900

    add: スタイルの指定

--- html/thanks.php
+++ html/thanks.php
@@ -11,6 +11,7 @@ $password = $_POST['password'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>送信完了</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
