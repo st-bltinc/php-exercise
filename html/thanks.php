863c92876c2398d9ea561b66d2fb9debf9ef18aa
stblt <staniguchi@bltinc.co.jp>
2026-04-24 17:11:00 +0900

    feat: hanks.phpの実装

---
+++ html/thanks.php
@@ -0,0 +1,22 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>送信完了</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <p>名前: <?php echo htmlspecialchars($name); ?></p>
+    <p>メールアドレス: <?php echo htmlspecialchars($email); ?></p>
+    <p>パスワード: <?php echo htmlspecialchars($password); ?></p>
+</body>
+
+</html>
