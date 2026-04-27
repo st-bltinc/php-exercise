20fb465219e27b485eb4104e56c9c56d9a990d4c
stblt <staniguchi@bltinc.co.jp>
2026-04-27 15:02:26 +0900

    feat: バリデーションの実装

--- html/index.php
+++ html/index.php
@@ -1,15 +1,36 @@
+<?php
+session_start();
+$errors = $_SESSION['errors'] ?? [];
+unset($_SESSION['errors']);
+?>
 <!DOCTYPE html>
 <html>
 
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
+    <title>お問い合わせ</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <?php phpinfo(); ?>
+    <?php if (!empty($errors)): ?>
+        <ul>
+            <?php foreach ($errors as $e): ?>
+                <li><?= htmlspecialchars($e, ENT_QUOTES, 'UTF-8') ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+    <form method="POST" action="thanks.php">
+        <label for="name">名前</label><br>
+        <input type="text" id="name" name="name"><br>
+        <label for="email">メールアドレス</label><br>
+        <input type="email" id="email" name="email"><br>
+        <label for="password">パスワード</label><br>
+        <input type="password" id="password" name="password"><br>
+        <button type="submit" value="送信">送信</button>
+    </form>
 </body>
 
 </html>
--- html/thanks.php
+++ html/thanks.php
@@ -1,7 +1,39 @@
 <?php
+session_start();
+
 $name = $_POST['name'];
 $email = $_POST['email'];
 $password = $_POST['password'];
+$errors = [];
+
+if ($name === '') {
+    $errors[] = '名前を入力してください。';
+} else {
+    if (mb_strlen($name) < 3) {
+        $errors[] = '名前は三文字以上で入力してください。';
+    }
+}
+
+if ($password === '') {
+    $errors[] = 'パスワードを入力してください。';
+} else {
+    if (mb_strlen($password) < 5) {
+        $errors[] = 'パスワードは五文字以上で入力してください。';
+    }
+}
+
+if ($email === '') {
+    $errors[] = 'メールアドレスを入力してください。';
+}
+
+if (!empty($errors)) {
+    $_SESSION['errors'] = $errors;
+    header('Location: index.php');
+    exit;
+}
+
+
+
 ?>
 <!DOCTYPE html>
 <html>
